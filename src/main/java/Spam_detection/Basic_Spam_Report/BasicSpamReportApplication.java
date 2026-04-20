6fa5770ae0fb07ea2f8f80efe9939ae4173efeb1
AbhinavPratyush <abhinavpratyush273@gmail.com>
2026-04-20 15:53:13 +0530

    initial commit

---
+++ src/main/java/Spam_detection/Basic_Spam_Report/BasicSpamReportApplication.java
@@ -0,0 +1,13 @@
+package Spam_detection.Basic_Spam_Report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpamReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpamReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spam_detection/Basic_Spam_Report/BasicSpamReportApplicationTests.java
@@ -0,0 +1,13 @@
+package Spam_detection.Basic_Spam_Report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpamReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
